8987b37e20a1aa22809b423141129d1d3b8af34a
Anemosina <teodora.ubuntu@gmail.com>
2021-04-13 14:21:58 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!--Bootstrap css-->
+  <link rel="stylesheet" href="css/bootstrap.css" >
+  <title>Document</title>
+</head>
+<body>
+<div class="container">
+<button type="button" class="btn btn-primary">Default</button>
+</div>
+  <!--jQuery-->
+  <script src="js/jquery.min.js" charset="UTF-8"></script>
+  <!--Javascrip-->
+  <script src="js/bootstrap.bundle.js" charset="UTF-8"></script>
+
+</body>
+</html>
